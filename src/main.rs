7927779bf23196620ce09f24f9b31821e84ffa61
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2024-01-18 18:04:21 +0100

    👷 Hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::get;
+use actix_web::App;
+use actix_web::HttpServer;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(hello))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
+}
+
+#[get("/")]
+async fn hello() -> &'static str {
+    "Hello world!"
 }
