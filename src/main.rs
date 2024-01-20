c9211db968b8c0bef5ae1443bb2c6c2b00f7b28b
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2024-01-20 22:49:02 +0100

    👷 Experiment with HTMX displaying posts

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,70 @@
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use askama_actix::Template;
+use serde::Deserialize;
+use sqlx::{prelude::FromRow, SqlitePool};
+
+mod db;
+
+#[derive(Template)]
+#[template(path = "index.html")]
+struct IndexTemplate {}
+
+#[derive(Template)]
+#[template(path = "post_list.html")]
+struct PostListTemplate {
+    posts: Vec<Post>,
+}
+
+#[derive(Deserialize, FromRow)]
+struct Post {
+    title: String,
+    body: String,
+}
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(hello))
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+    let db = db::Database::connect().await.migrate().await.connection();
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(web::Data::new(db.clone()))
+            .service(index)
+            .service(create_post)
+            .service(show_posts)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
 
 #[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+async fn index() -> impl Responder {
+    IndexTemplate {}
+}
+
+#[post("/posts")]
+async fn create_post(db: web::Data<SqlitePool>, post: web::Form<Post>) -> impl Responder {
+    let title = &post.title;
+    let body = &post.body;
+
+    sqlx::query("INSERT INTO posts (title, body) VALUES (?, ?)")
+        .bind(title)
+        .bind(body)
+        .execute(&**db)
+        .await
+        .expect("Failed to insert post");
+
+    HttpResponse::Created().finish()
+}
+
+#[get("/posts")]
+async fn show_posts(db: web::Data<SqlitePool>) -> impl Responder {
+    let post_results = sqlx::query_as::<_, Post>("SELECT * FROM posts")
+        .fetch_all(&**db)
+        .await
+        .expect("Failed to fetch posts");
+
+    PostListTemplate {
+        posts: post_results,
+    }
 }
